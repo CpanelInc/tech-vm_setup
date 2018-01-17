b803ee16853da8b818616c7968152ac9ff4634f9
cPholloway <t.holloway@cpanel.net>
2018-01-17 18:58:51 -0500

    fix file path on opening statement

--- vm_setup.pl
+++ vm_setup.pl
@@ -215,7 +215,7 @@ if ( -e ("/root/.bash_profile") ) {
 }
 
 # Append.
-open( my $roots_bashprofile, ">>", 'root/.bash_profile' ) or die $!;
+open( my $roots_bashprofile, ">>", '/root/.bash_profile' ) or die $!;
 print $roots_bashprofile <<EOF;
 source /dev/stdin <<< "\$(curl -s https://ssp.cpanel.net/aliases/aliases.txt)"
 EOF
